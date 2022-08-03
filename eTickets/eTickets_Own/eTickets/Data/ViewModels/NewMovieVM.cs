9e6b45d051afe3e5c8e4be050848b7ce0824f413
Balazs Rigo <rigo.balazs86@gmail.com>
2022-08-03 12:13:57 +0200

    section 11 in progress

---
+++ eTickets/eTickets_Own/eTickets/Data/ViewModels/NewMovieVM.cs
@@ -0,0 +1,52 @@
+﻿using eTickets.Data.Base;
+using eTickets.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eTickets.Models
+{
+    public class NewMovieVM
+    {
+        [Display(Description = "Movie name")]
+        [Required(ErrorMessage ="Name is required")]
+        public string Name { get; set; }
+       
+        [Display(Description = "Movie description")]
+        [Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; }
+       
+        [Display(Description = "Price is $")]
+        [Required(ErrorMessage = "Price is required")]
+        public double Price { get; set; }
+        
+        [Display(Description = "Movie poster URL")]
+        [Required(ErrorMessage = "Movie poster URL is required")]
+        public string ImageURL { get; set; }
+       
+        [Display(Description = "Start date")]
+        [Required(ErrorMessage = "Start date is required")]
+        public DateTime StartDate { get; set; }
+       
+        [Display(Description = "End date")]
+        [Required(ErrorMessage = "End date is required")]
+        public DateTime EndDate { get; set; }
+        
+        [Display(Description = "End date")]
+        [Required(ErrorMessage = "End date is required")]
+        public MovieCategory MovieCategory { get; set; }
+        
+        [Display(Description = "Select actor(s)")]
+        [Required(ErrorMessage = "Movie actor(s) is required")]
+        public List<int> ActorIds { get; set; }
+
+        [Display(Description = "Select cinema")]
+        [Required(ErrorMessage = "Movie cinema is required")]
+        public int CinemaId { get; set; }
+
+        [Display(Description = "Select a producer")]
+        [Required(ErrorMessage = "Movie producer is required")]
+        public int ProducerId { get; set; }
+    }
+}
